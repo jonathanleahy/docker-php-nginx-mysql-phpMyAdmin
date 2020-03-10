9f916948650e56316da95175724248a7a82cebc1
Jonathan Leahy <jonathanl@newtimesystems.com>
2020-03-10 13:29:23 +0000

    http and command response

--- www/default/index.php
+++ www/default/index.php
@@ -1,17 +1,41 @@
 <?php
 
 use App\Vegetables;
+use MiladRahimi\PhpRouter\Exceptions\RouteNotFoundException;
 use MiladRahimi\PhpRouter\Router;
+use Zend\Diactoros\Response\HtmlResponse;
 
 require_once('./vendor/autoload.php');
 
-$router = new Router();
+$listveg = false;
 
-$router->get('/vegetables', function () {
+if (isset($argc)) {
+    $arguments = (array_count_values($argv));
+    if ($arguments['--list-veg']) {
+        $listveg = true;
+    }
+}
+
+if (!$listveg) {
+
+    $router = new Router();
+
+    $router->get('/vegetables', function () {
+        $a = new Vegetables();
+        return (JSON_encode($a->populate()));
+    });
+
+    try {
+        $router->dispatch();
+    } catch (RouteNotFoundException $e) {
+        $router->getPublisher()->publish(new HtmlResponse('Not found.', 404));
+    } catch (Throwable $e) {
+        $router->getPublisher()->publish(new HtmlResponse('Internal error.', 500));
+    }
+
+} else {
+
     $a = new Vegetables();
-    return (JSON_encode($a->populate()));
-});
+    echo (JSON_encode($a->populate()));
 
-$router->dispatch();
-
-
+}
